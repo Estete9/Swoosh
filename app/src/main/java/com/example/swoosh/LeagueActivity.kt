ee04c33e438f7226719c8519d7ca61427200c174
Esteban P <cepalacios@outlook.com>
2019-08-27 14:44:45 -0500

    league activity

--- app/src/main/java/com/example/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/LeagueActivity.kt
@@ -3,10 +3,10 @@ package com.example.swoosh
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class WelcomeActivity : AppCompatActivity() {
+class LeagueActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+        setContentView(R.layout.activity_league)
     }
 }
