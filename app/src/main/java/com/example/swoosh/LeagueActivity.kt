bd593d9074d7ed959f853c7274d7f422f287589d
Esteban P <cepalacios@outlook.com>
2019-08-29 07:24:09 -0500

    skill layout and second way of making an intent

--- app/src/main/java/com/example/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/LeagueActivity.kt
@@ -1,12 +1,23 @@
 package com.example.swoosh
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 
 class LeagueActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+
+//        toSkillBtn.setOnClickListener {
+//            val skillIntent = Intent(this, SkillActivity::class.java)
+//            startActivity(skillIntent)
+//onclicklistener method of intent
+    }
+
+    fun leagueNextClicked(view: View) {
+        val skillActivity = Intent(this, SkillActivity::class.java)
+        startActivity(skillActivity)
     }
 }
